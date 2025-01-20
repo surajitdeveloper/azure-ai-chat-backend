2f0ddf12d09355375676ab5e653b1df7aa90712a
Surajit <surajit1992@live.com>
2025-01-20 22:52:14 +0530

    ai socket

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { Server } = require("socket.io");
 const axios = require('axios');
 
 const aiEndpoint = "https://ai-surajitai526010179468.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview"
-const apiKey = ""
+const apiKey = "9sSLE88DivbuRjsEsM84duv8AcCdMICrdz2i3FvvTC6JeCzvq9NnJQQJ99BAACHYHv6XJ3w3AAAAACOGqKKE"
 
 const PORT = process.env.PORT || 4000; 
 
@@ -44,11 +44,25 @@ io.on("connection", (socket) => {
     console.log("User connected ", socket.id); 
 
 
-    socket.on("send_message", (data) => {
+    socket.on("send_message", async (data) => {
         console.log("Message Received ", data); 
-
+        if(data.messages.length === 0) return
+        const response = await axios.post(aiEndpoint, {
+          messages: data.messages,
+          temperature: 0.7,
+          max_tokens: 100
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'api-key': apiKey
+            }
+        });
+        console.log("Response from AI ", response?.data?.choices[0]?.message);
       
-        io.emit("receive_message", { clientId: data.clientId, messages: "hii" });
+        io.emit("receive_message", { 
+          clientId: data.clientId, 
+          query: data.messages[0].content, 
+          response: response?.data?.choices?.length > 0 ? response?.data?.choices[0]?.message?.content : '' });
     });
 });
 
