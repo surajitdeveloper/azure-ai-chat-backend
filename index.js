3692756f7d7bb3b60eb13b94cb57ba53077bfb74
Surajit <surajit1992@live.com>
2025-01-25 10:28:08 +0530

    serve

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 const axios = require("axios");
-const fs = require('node:fs');
+const fs = require("node:fs");
 
 const aiEndpoint =
   "https://ai-surajitai526010179468.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview";
 const apiKey =
   "9sSLE88DivbuRjsEsM84duv8AcCdMICrdz2i3FvvTC6JeCzvq9NnJQQJ99BAACHYHv6XJ3w3AAAAACOGqKKE";
+
+// const origin = ["https://surajitdeveloper.github.io", "http://localhost:3000"];
+const origin =  ["https://surajitdeveloper.github.io"]
 
 const PORT = process.env.PORT || 4000;
 
@@ -21,11 +24,11 @@ app.use(express.json());
 
 const server = http.createServer(app);
 
-const filePath = './logs.log';
+const filePath = "./logs.log";
 
 const io = new Server(server, {
   cors: {
-    origin: "https://surajitdeveloper.github.io",
+    origin: origin,
     methods: ["GET", "POST"],
   },
 });
@@ -35,14 +38,20 @@ app.get("/", (req, res) => {
 });
 
 app.get("/logs", (req, res) => {
-  res.send(fs.readFileSync(filePath, 'utf8'));
-})
+  res.send(fs.readFileSync(filePath, "utf8")?.replace(/\n/g, "<br />"));
+});
 
 io.on("connection", (socket) => {
-  // console.log("User connected ", socket.id);
   socket.on("send_message", async (data) => {
-    // console.log("Message Received ", data);
+    console.log("Message Received ", data);
     if (data.messages.length === 0) return;
+    if (
+      !origin.reduce((a, elm) => {
+        if (a || socket.handshake.headers.referer.includes(elm)) a = true;
+        return a;
+      }, false)
+    )
+      return;
     const response = await axios.post(
       aiEndpoint,
       {
@@ -65,17 +74,19 @@ io.on("connection", (socket) => {
         response?.data?.choices?.length > 0
           ? response?.data?.choices[0]?.message?.content
           : "",
-    }
+    };
     io.emit("receive_message", responseData);
     const log = `
-    \n ---------------------\n <br />
     Client ID: ${data.clientId} | 
     Query: ${data.messages[0].content} | 
-    Response: ${response?.data?.choices?.length > 0 ? response?.data?.choices[0]?.message?.content: ""} | 
+    Response: ${
+      response?.data?.choices?.length > 0
+        ? response?.data?.choices[0]?.message?.content
+        : ""
+    } | 
     Time Stamp: ${new Date()}
-    \n ---------------------\n`;
+    \n`;
     fs.appendFileSync(filePath, log);
-
   });
 });
 
