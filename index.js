e0715868a4ea4f239afce1ca8ad81fccb13fd9af
Surajit <surajit1992@live.com>
2025-02-15 09:17:06 +0530

    hjhj

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ const axios = require("axios");
 const fs = require("node:fs");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const aiEndpoint =
-  "https://ai-surajitai526010179468.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview";
-const apiKey =
-  "9sSLE88DivbuRjsEsM84duv8AcCdMICrdz2i3FvvTC6JeCzvq9NnJQQJ99BAACHYHv6XJ3w3AAAAACOGqKKE";
+// const aiEndpoint =
+//   "https://ai-surajitai526010179468.openai.azure.com/openai/deployments/gpt-4/chat/completions?api-version=2024-08-01-preview";
+
+const aiEndpoint = "http://65.21.248.37:8181/v1/chat/completions";
+
+// const apiKey =
+//   "9sSLE88DivbuRjsEsM84duv8AcCdMICrdz2i3FvvTC6JeCzvq9NnJQQJ99BAACHYHv6XJ3w3AAAAACOGqKKE";
 
   const genAI = new GoogleGenerativeAI("AIzaSyCp5l6GGuZ_IKIJwolnQ0pYbR__FhGBAt8");
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-// const origin = ["https://surajitdeveloper.github.io", "http://localhost:3000"];
-const origin =  ["https://surajitdeveloper.github.io"];
+const origin = ["https://surajitdeveloper.github.io", "http://localhost:3000"];
+// const origin =  ["https://surajitdeveloper.github.io"];
 
 const PORT = process.env.PORT || 4000;
 
@@ -70,16 +73,28 @@ app.get("/logs", (req, res) => {
 });
 
 io.on("connection", (socket) => {
+  console.log("Socket connected ", socket.id);
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected ", socket.id);
+  });
   socket.on("send_message", async (data) => {
+    console.log("Socket referer ", socket.handshake.headers);
     console.log("Message Received ", data);
     if (data.messages.length === 0) return;
-    if (
-      !origin.reduce((a, elm) => {
-        if (a || socket.handshake.headers.referer.includes(elm)) a = true;
-        return a;
-      }, false)
-    )
-      return;
+    // if (
+    //   !origin.reduce((a, elm) => {
+    //     if (a || socket.handshake.headers.referer.includes(elm)) a = true;
+    //     return a;
+    //   }, false)
+    // )
+    //   return;
+
+    console.log("axios conf --->", aiEndpoint,
+      {
+        messages: data.messages,
+        temperature: 0.7,
+        max_tokens: 100,
+      })
     const response = await axios.post(
       aiEndpoint,
       {
@@ -90,11 +105,11 @@ io.on("connection", (socket) => {
       {
         headers: {
           "Content-Type": "application/json",
-          "api-key": apiKey,
+          // "api-key": apiKey,
         },
       }
     );
-    // console.log("Response from AI ", response?.data?.choices[0]?.message);
+    console.log("Response from AI ", response?.data?.choices[0]?.message);
     const responseData = {
       clientId: data.clientId,
       query: data.messages[0].content,
